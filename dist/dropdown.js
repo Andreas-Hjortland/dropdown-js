07414875edebc93d0074e0ca286f248a2050684b
Andreas Hjortland <git@hjortland.org>
2017-01-27 15:48:38 +0100

    Added dist file which works with IE

---
+++ dist/dropdown.js
@@ -0,0 +1,192 @@
+'use strict';
+
+if (!NodeList.prototype.forEach) {
+	NodeList.prototype.forEach = Array.prototype.forEach;
+}
+
+var debug = function debug() {}; // console.log.bind(console);
+function closeRelated(subnav) {
+	subnav.parentElement.querySelectorAll('.open').forEach(function (subnav) {
+		subnav.classList.remove('open');
+	});
+}
+function openNested(subnav) {
+	var xPos = '95%';
+	var yPos = '-0.2em';
+	closeRelated(subnav);
+	subnav.classList.add('open');
+	var ul = subnav.querySelector('ul');
+	var rect = subnav.getBoundingClientRect();
+	var ulRect = ul.getBoundingClientRect();
+	if (rect.top + ulRect.height > window.innerHeight) {
+		ul.style.top = '';
+		ul.style.bottom = yPos;
+	} else {
+		ul.style.top = yPos;
+		ul.style.bottom = '';
+	}
+	if (rect.right + ulRect.width > window.innerWidth) {
+		ul.style.left = '';
+		ul.style.right = xPos;
+	} else {
+		ul.style.left = xPos;
+		ul.style.right = '';
+	}
+}
+function navClick(e) {
+	debug(e.target);
+	if (e.target.classList.contains('subnav')) {
+		openNested(e.target);
+		e.stopPropagation();
+	}
+}
+
+var elts = document.querySelectorAll('[data-dropdown-trigger]');
+elts.forEach(function (elt) {
+	return elt.addEventListener('click', function (evt) {
+		evt.stopPropagation();
+		var id = elt.getAttribute('data-dropdown-trigger');
+		var nav = document.getElementById(id);
+		nav.querySelectorAll('.open,.active').forEach(function (elt) {
+			elt.classList.remove('open', 'active');
+		});
+		nav.classList.add('opened');
+		if (evt.clientX && evt.clientY) {
+			var rect = nav.getBoundingClientRect();
+			if (rect.width + evt.clientX > window.innerWidth) {
+				nav.style.left = '';
+				nav.style.right = window.innerWidth - evt.clientX + 'px';
+			} else {
+				nav.style.left = evt.clientX + 'px';
+				nav.style.right = '';
+			}
+			nav.style.top = window.scrollY + evt.clientY + 'px';
+			nav.style.bottom = '';
+			console.log(rect.height + evt.clientY, window.innerHeight);
+			nav.style.transform = '';
+			if (rect.height + evt.clientY > window.innerHeight) {
+				nav.style.transform = 'translateY(-100%)';
+			}
+		} else {
+			var _rect = elt.getBoundingClientRect();
+			debug(_rect);
+			nav.style.top = _rect.right + 'px';
+			nav.style.left = _rect.bottom + 'px';
+		}
+		debug({ id: id, nav: nav });
+
+		var timeout = void 0;
+		function mouseEnter(e) {
+			debug('mouseenter', e.target);
+
+			e.target.classList.add('active');
+
+			clearTimeout(timeout);
+			if (e.target.classList.contains('subnav')) {
+				timeout = setTimeout(openNested.bind(null, e.target), 500);
+			} else {
+				timeout = setTimeout(closeRelated.bind(null, e.target), 500);
+			}
+		}
+		function mouseLeave(e) {
+			clearTimeout(timeout);
+			e.target.classList.remove('active');
+			debug('mouseleave', e.target);
+		}
+		function keyboardNavigation(e) {
+			var open = nav.querySelectorAll('.open > ul');
+			if (open.length > 0) {
+				open = open[open.length - 1];
+			} else {
+				open = nav;
+			}
+			var active = open.querySelector('li.active');
+			debug(open);
+			var next = void 0;
+			switch (e.keyCode) {
+				case 38:
+					// up arrow
+					e.stopPropagation();
+					e.preventDefault();
+					if (active) {
+						next = active.previousElementSibling;
+						active.classList.remove('active');
+					}
+					if (!next) {
+						next = open.children[open.children.length - 1];
+					}
+					next.classList.add('active');
+					break;
+				case 40:
+					// down arrow
+					e.stopPropagation();
+					e.preventDefault();
+					if (active) {
+						next = active.nextElementSibling;
+						active.classList.remove('active');
+					}
+					if (!next) {
+						next = open.children[0];
+					}
+					next.classList.add('active');
+					break;
+				case 37:
+					// left arrow
+					e.stopPropagation();
+					e.preventDefault();
+					if (active) {
+						active.classList.remove('active');
+						active.parentElement.parentElement.classList.remove('open');
+						active.parentElement.parentElement.classList.add('active');
+					} else {
+						if (open && open !== nav) {
+							closeRelated(open.parentElement);
+						}
+					}
+					break;
+				case 39:
+					// right arrow
+					e.stopPropagation();
+					e.preventDefault();
+					if (active) {
+						openNested(active);
+						active.querySelector('li').classList.add('active');
+					}
+					break;
+				case 13:
+					// enter
+					e.stopPropagation();
+					e.preventDefault();
+					if (active) {
+						active.click();
+					}
+					break;
+				case 27:
+					dismiss();
+					break;
+			}
+		}
+
+		function dismiss() {
+			nav.classList.remove('opened');
+			document.removeEventListener('keypress', keyboardNavigation);
+			document.removeEventListener('click', dismiss);
+
+			nav.removeEventListener('click', navClick);
+
+			nav.querySelectorAll('li').forEach(function (subnav) {
+				subnav.classList.remove('open', 'active');
+				subnav.removeEventListener('mouseenter', mouseEnter);
+				subnav.removeEventListener('mouseleave', mouseLeave);
+			});
+		}
+
+		nav.querySelectorAll('li').forEach(function (subnav) {
+			subnav.addEventListener('mouseenter', mouseEnter);
+			subnav.addEventListener('mouseleave', mouseLeave);
+		});
+		nav.addEventListener('click', navClick);
+		document.addEventListener('keydown', keyboardNavigation);
+		document.addEventListener('click', dismiss);
+	});
+});
